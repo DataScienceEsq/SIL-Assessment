0f80286c3881b8032f7dd86f4ff4abaa21314d40
Robert Ferdinand <UOFLOUISIANA@GMAIL.COM>
2021-02-15 15:56:34 -0600

    Update SIL-Task3.py

--- SIL-Task3.py
+++ SIL-Task3.py
@@ -2,16 +2,16 @@
 
 Author: Robert Ferdinand (2021)
 
-This PYTHON program reads in a CSV file 'out.csv' and outputs a database which can be queried using SQL.
+This PYTHON program reads a CSV file 'out.csv' as input and outputs a database 'bibleSource' which can be queried using SQL.
+
 To run this program, please note the following:
 
-1. The file 'out.csv' needs to be present in your folder or a path needs to provided to the file.
+1. The file 'out.csv' needs to be present in local folder or a path to the file needs to provided in the 'pd.read_csv' statement below.
 2. From the command prompt one can run the program using the command 'python SIL-Task3.py'.
 
 
 '''
 
-import sqlalchemy
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -23,7 +23,7 @@ engine = create_engine('sqlite://', echo=False)
 
 df.to_sql('bibleSource', con=engine)
 
-# Running SQL Query to Count Number of Records in the database bibleSource
+# Running SQL Query to Count Number of Records in the database 'bibleSource'
 
 a = engine.execute("SELECT count(*) FROM bibleSource").fetchall()[0][0]
 
