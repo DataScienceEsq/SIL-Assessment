a3b30d9d6de60b8d140d95a938e47e60dc0aa53d
Robert Ferdinand <UOFLOUISIANA@GMAIL.COM>
2021-02-15 17:35:55 -0600

    Add files via upload

--- SIL-Task1&2.py
+++ SIL-Task1&2.py
@@ -2,12 +2,12 @@
 
 Author: Robert Ferdinand (2021)
 
-This PYTHON program parses a BibTex file called 'source.bib' and outputs a CSV file called 'out.csv'.
+This PYTHON 3.7 program parses a BibTex file called 'source.bib' and outputs a large CSV file called 'out.csv'.
 
 To run this program, please note the following:
 
-1. The file 'source.bib' needs to be present in local folder or a path to the file needs to provided in OUTFILE.
-2. From the command prompt one can run the program using the command 'python SIL-Task1&2.py'.
+1. The file 'source.bib' needs to be present in the local folder or a path to the file needs to be provided in OUTFILE.
+2. From the command prompt, one can run the program using the command 'python py SIL-Task1&2.py'.
 
 
 '''
@@ -31,10 +31,13 @@ for bib_id in bibdata.entries:
     try:
         TITLE = str(b['title'])
         TITLE = TITLE.replace(',','')
+        TITLE = TITLE.replace('"','')
         LGCODE = str(b['lgcode'])
         LGCODE = LGCODE.replace(',','')
+        LGCODE = LGCODE.replace('"','')
         YEAR = str(b['year'])
         YEAR = YEAR.replace(',','')
+        YEAR = YEAR.replace('"','')
         fh.write(str(bib_id)+","+TITLE+","+LGCODE+","+YEAR+"\n")
     except(KeyError, UnicodeEncodeError):
         fh.write(str(bib_id)+","+" "+","+" "+","+" "+"\n")
--- SIL-Task3.py
+++ SIL-Task3.py
@@ -2,12 +2,12 @@
 
 Author: Robert Ferdinand (2021)
 
-This PYTHON program reads a CSV file 'out.csv' as input and outputs a database 'bibleSource' which can be queried using SQL.
+This PYTHON 3.7 program reads a CSV file 'out.csv' and outputs a database 'bibleSource' which can be queried using SQL.
 
 To run this program, please note the following:
 
-1. The file 'out.csv' needs to be present in local folder or a path to the file needs to provided in the 'pd.read_csv' statement below.
-2. From the command prompt one can run the program using the command 'python SIL-Task3.py'.
+1. The file 'out.csv' needs to be present in the local folder or a path to the file needs to be provided.
+2. From the command prompt, one can run the program using the command 'python SIL-Task3.py'.
 
 
 '''
@@ -17,14 +17,12 @@ from sqlalchemy import create_engine
 
 df = pd.read_csv('out.csv', engine = "python", sep = ',', quotechar = '"', error_bad_lines = False)
 
-print(df.shape)
-
 engine = create_engine('sqlite://', echo=False)
 
 df.to_sql('bibleSource', con=engine)
 
 # Running SQL Query to Count Number of Records in the database 'bibleSource'
 
-a = engine.execute("SELECT count(*) FROM bibleSource").fetchall()[0][0]
+a = engine.execute("SELECT COUNT(*) FROM bibleSource").fetchall()
 
-print('Total Number of Records in the bibleSource Database = %d' % a)
+print('Total Number of Records in the bibleSource Database = %d' % a[0][0])
